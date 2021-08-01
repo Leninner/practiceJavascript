2f7ff80e1016fa12d7bc095c01df82c12d07f86f
leninner <mazabandalenin180@gmail.com>
2021-08-01 13:39:19 -0500

    Añado los elementos principales para el proyecto

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # practiceJavascript
+
+..
+
+## Taller #1: Figuras Geométricas
+
+-   1. Definir las fórmulas
+-   2. Implementar fórmulas en Javascript
+-   3. Crear funciones
+-   4. Integrar Javascript con HTML
---
+++ figuras.js
@@ -0,0 +1,19 @@
+console.group("Cuadrados");
+
+function resumenCuadrado(lado) {
+    console.log(lado * 4 + " cm");
+    console.log(lado ** 2 + " cm´2");
+}
+resumenCuadrado(7);
+
+console.groupEnd();
+
+console.group("Triángulos");
+
+function resumenTriangulo(ladoOne, ladoTwo, base, altura) {
+    console.log(ladoOne + ladoTwo + base + " cm");
+    console.log((base * altura) / 2 + " cm2");
+}
+resumenTriangulo(10, 10, 5, 9);
+
+console.groupEnd();
