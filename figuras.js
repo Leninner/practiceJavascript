b2a6898c1b889b88ce371643203b5123b044aabc
leninner <mazabandalenin180@gmail.com>
2021-08-01 20:12:41 -0500

    Añado las funciones de todas las figuras geométricas

--- figuras.js
+++ figuras.js
@@ -1,3 +1,5 @@
+// Cuadrado
+
 console.group("Cuadrados");
 
 function resumenCuadrado(lado) {
@@ -8,6 +10,8 @@ resumenCuadrado(7);
 
 console.groupEnd();
 
+// Triángulos
+
 console.group("Triángulos");
 
 function resumenTriangulo(ladoOne, ladoTwo, base, altura) {
@@ -15,5 +19,17 @@ function resumenTriangulo(ladoOne, ladoTwo, base, altura) {
     console.log((base * altura) / 2 + " cm2");
 }
 resumenTriangulo(10, 10, 5, 9);
+
+console.groupEnd();
+
+// Círculos
+
+console.group("Circulos");
+
+function resumenCirculo(diametro) {
+    console.log(Math.PI * diametro + " cm");
+    console.log((diametro / 2) ** 2 * Math.PI + " cm´2");
+}
+resumenCirculo(8);
 
 console.groupEnd();
