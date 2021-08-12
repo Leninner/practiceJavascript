957fab3771414d47ba77e7dfc7afbd7b2182c00b
leninner <mazabandalenin180@gmail.com>
2021-08-11 22:10:05 -0500

    Añado eventos de escucha en los botones y refactorizo el código de los favicons

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
-const btnSeeMore = document.querySelector("#btnSeeMore");
+const btnSeeMore = document.querySelector("#btnSeeMore"),
+    btnDemoGeometria = document.querySelector("#demoGeometria"),
+    btnRepoGeometria = document.querySelector("#repoGeometria"),
+    btnDemoPorcentajes = document.querySelector("#demoPorcentajes"),
+    btnRepoPorcentajes = document.querySelector("#repoPorcentajes"),
+    btnDemoSalarios = document.querySelector("#demoSalarios"),
+    btnRepoSalarios = document.querySelector("#repoSalarios");
 
-btnSeeMore.addEventListener("click", () => {
-    location.hash = "#geometria__info"; // NOTE: Location.hash sirve para movernos entre secciones de una página web.
-});
+btnSeeMore.onclick = () => (location.hash = "#geometria__info"); // NOTE: Location.hash sirve para movernos entre secciones de una página web.
+
+btnDemoGeometria.onclick = () => {
+    window.open("./shapes/index.html", "_blank");
+};
+btnRepoGeometria.onclick = () => {
+    window.open("https://github.com/Leninner/practiceJavascript/tree/main/shapes", "_blank");
+};
+
+btnDemoPorcentajes.onclick = () => {
+    window.open("./percents/index.html", "_blank");
+};
+btnRepoPorcentajes.onclick = () => {
+    window.open("https://github.com/Leninner/practiceJavascript/tree/main/percents", "_blank");
+};
+
+btnDemoSalarios.onclick = () => {
+    window.open("./salarios/index.html", "_blank");
+};
+btnRepoSalarios.onclick = () => {
+    window.open("https://github.com/Leninner/practiceJavascript/tree/main/salarios", "_blank");
+};
